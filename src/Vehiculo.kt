99a2664df95267eea1f5ba3fc5d4fffe18600163
ilopjim13 <ivanlj@iesrafaelalberti.es>
2024-02-15 16:20:59 +0100

    creado en clase

---
+++ src/Vehiculo.kt
@@ -0,0 +1,16 @@
+class Vehiculo(val marca:String, val modelo: String, val capacidadCombustible: Float, val combustibleActual: Float, val kilometrosActuales:Int) {
+
+    fun obtenerInformacion(): String {
+        return "El vehiculo puede recorrer ${combustibleActual * 10} kilometros con $combustibleActual litros de combustible."
+    }
+
+    fun calcularAutonomia() = capacidadCombustible * 10
+
+    fun realizarViaje(distancia:Int) :Int {
+
+    }
+
+    fun repostar(cantidad:Float) :Float {
+
+    }
+}
